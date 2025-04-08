d894671f92b49c12016624ae828ee8fd2d22f654
VirtualGamesOfficial <DK30640@hotmail.com>
2025-04-08 18:09:27 +0200

    Update code-editor.js

--- code-editor.js
+++ code-editor.js
@@ -11,6 +11,16 @@ window.addEventListener("DOMContentLoaded", () => {
     });
 });
 
+require.config({ paths: { 'vs': 'https://cdnjs.cloudflare.com/ajax/libs/monaco-editor/0.34.1/min/vs' } });
+require(['vs/editor/editor.main'], function () {
+    const editor = monaco.editor.create(document.getElementById('editor'), {
+        value: '',
+        language: 'javascript',
+        theme: 'vs-dark'
+    });
+});
+
+
 function downloadCode() {
     const codeContent = document.getElementById("editor").value;
     const blob = new Blob([codeContent], { type: "text/html" });
